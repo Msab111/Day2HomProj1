c7df706f0e320c5753e504627f732b1672811def
sabamansoori <sabamansoori@10.0.0.102>
2023-04-10 16:06:21 -0400

    initial commit

---
+++ src/day2hwp1/Day2Hwp1Dem.java
@@ -0,0 +1,24 @@
+package day2hwp1;
+
+public class Day2Hwp1Dem {
+
+	
+	
+public static void main (String args []) {
+		
+		System.out.println ("Day 2 hwone stash ");
+		
+	}
+
+
+public void run() {
+	
+	System.out.println ("Rnning.....");
+
+
+}
+	
+	
+	
+	
+}
